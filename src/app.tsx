98bf10539fdcf3d9b2e94d5ff37a20d78a519984
Joao Guerra <jvggj@live.com>
2025-03-31 16:13:15 -0300

    fix: remove unwanted box grid

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
-import { BooksProvider } from './books';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
+import { BooksProvider } from "./books";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 function App() {
   return (
